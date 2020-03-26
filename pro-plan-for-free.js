45b4ab3e26d13dcf67a6e2501c8f05ba1404aa84
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-03-26 17:12:08 -0400

    Add poollovernathan for free

--- pro-plan-for-free.js
+++ pro-plan-for-free.js
@@ -9,5 +9,6 @@ module.exports = [
   'itspugle', // 2018-10-24: Student creating tools to better enable volunteer groups,
   'urbanengine', // 2019-03-29: non-profit organization helping startups & small businesses network and grow within their community
   'moonsmile', // 2019-9-1: Student writing tools to learn, I promise i will pay when I finish my project.
-  'apache' // 2020-01-01: non-profit corporation to support Apache software projects
+  'apache', // 2020-01-01: non-profit corporation to support Apache software projects
+  'poollovernathan' // 2020-03-26: Head (and only) programmer in my 5th-grade class. If I had a credit card, I would be able to pay, but I don't, and cash can't exactly be scanned onto the internet
 ]
